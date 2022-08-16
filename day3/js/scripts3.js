7ad3af46ac815681f97a9fb9a04d0c4822bb895d
HellNouez <105078178+HellNOuez@users.noreply.github.com>
2022-08-15 19:09:46 -0700

    Add files via upload

---
+++ day3/js/scripts3.js
@@ -0,0 +1,19 @@
+//somethingc
+const candy = "Reese's pieces";
+// console,log("supercalifrailisticeexpoalidocious")
+console.log(candy.length);
+
+console.log(435_543_534);
+const luckyNumber = 7;
+
+console.log(luckyNumber)
+console.log(GPA)
+
+const superHugeNumber = 90071992547409999n;
+console.log(superHugeNumber)
+const prefix = "new";
+const suffix = "york";
+console.log(prefix)
+console.log(suffix)
+
+// + (when used with strings, it concatenates)
